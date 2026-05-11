be50733840a73107c9a60317636a7cb408f7d44d
Jaira Cicat <jaira.cicat@gmail.com>
2026-05-11 13:16:45 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/jaira-finance/sw.js', { scope: '/jaira-finance/' })})}
